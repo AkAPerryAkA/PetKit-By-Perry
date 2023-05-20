232d57709977b09a6319483915961d36489f3de6
AkAPerryAkA <AkAPerryAkA@Gmail.com>
2023-05-20 13:50:11 +0000

    modified:   custom_components/petkit_by_perry/const.py

--- custom_components/petkit_by_perry/const.py
+++ custom_components/petkit_by_perry/const.py
@@ -1,9 +1,11 @@
+#from datetime import timedelta
+
 API_LOGIN_PATH = "user/login"
 API_DEVICES_PATH = "discovery/device_roster"
 API_DEVICE_DETAILS = "/owndevices"
 API_DEVICE_ACTIONS = "/controlDevice"
 API_REGION_SERVERS = "https://passport.petkt.com/v1/regionservers"
-API_SCAN_INTERVAL = datetime.datetime.timedelta(minutes=2)
+API_SCAN_INTERVAL = timedelta(minutes=2)
 DOMAIN = "petkit_by_perry"
 SUPPORTED_DOMAINS = [
     'sensor',
