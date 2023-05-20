b1d278b3434b120fe705db9b7c074c23ac523b6b
AkAPerryAkA <AkAPerryAkA@Gmail.com>
2023-05-20 14:00:44 +0000

    modified:   custom_components/petkit_by_perry/config_flow.py

--- custom_components/petkit_by_perry/config_flow.py
+++ custom_components/petkit_by_perry/config_flow.py
@@ -6,16 +6,14 @@ from homeassistant.helpers.selector import selector
 class PetKitByPerryConfigFlow(config_entries.ConfigFlow, domain = DOMAIN):
     async def async_step_user(self, user_input=None):
         # Specify items in the order they are to be displayed in the UI
-        data_schema = {
-            vol.Required("username"): str,
-            vol.Required("password"): str,
-        }
+        
+        STEP_USER_DATA_SCHEMA = vol.Schema(
+            {
+                vol.Required('Username'): TextSelector(TextSelectorConfig(type=TextSelectorType.EMAIL, autocomplete="username")),
+                vol.Required('Password'): TextSelector(TextSelectorConfig(type=TextSelectorType.PASSWORD, autocomplete="current-password")),
+                vol.Required("Country"): TextSelector(TextSelectorConfig(type=TextSelectorType.TEXT, autocomplete="country")),
+                vol.Required("TimeZone"): TextSelector(TextSelectorConfig(type=TextSelectorType.TEL, autocomplete="timezone")),
+            }
+        )
 
-        if self.show_advanced_options:
-            data_schema["allow_groups"] = selector({
-                "select": {
-                    "options": ["all", "light", "switch"],
-                }
-            })
-
-        return self.async_show_form(step_id="init", data_schema=vol.Schema(data_schema))
+        return self.async_show_form(step_id="init", data_schema=STEP_USER_DATA_SCHEMA)
