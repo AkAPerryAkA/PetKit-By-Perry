6e4fbf50b0e236d3e88b67a7aa79840b2dd504c4
AkAPerryAkA <AkAPerryAkA@Gmail.com>
2023-05-20 19:22:47 +0000

    modified:   custom_components/petkit_by_perry/config_flow.py

--- custom_components/petkit_by_perry/config_flow.py
+++ custom_components/petkit_by_perry/config_flow.py
@@ -24,7 +24,7 @@ class PetKitByPerryConfigFlow(config_entries.ConfigFlow, domain = DOMAIN):
             errors["base"] = "auth_error"
         
         API_SERVERS = []
-        Servers = await sendRequest(None, pytz.timezone(str(tzlocal.get_localzone())), locale.getdefaultlocale(), API_REGION_SERVERS)
+        Servers = sendRequest(None, pytz.timezone(str(tzlocal.get_localzone())), locale.getdefaultlocale(), API_REGION_SERVERS)
         for CountryCode in Servers:
             API_SERVERS.append([list(CountryCode.values())[2], list(CountryCode.values())[1]])
         
