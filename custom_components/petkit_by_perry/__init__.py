6553fc90c933179492c9f198ad2bb6b28e95fa5c
AkAPerryAkA <AkAPerryAkA@Gmail.com>
2023-05-20 13:28:32 +0000

    modified:   custom_components/petkit_by_perry/__init__.py

--- custom_components/petkit_by_perry/__init__.py
+++ custom_components/petkit_by_perry/__init__.py
@@ -1,8 +1,8 @@
 from .const import *
-from .Core import *
+#from .Core import *
 from .config_flow import *
-from .Account import *
-from .Device import *
+#from .Account import *
+#from .Device import *
 
 def setup(hass, config):
     """Set up is called when Home Assistant is loading our component."""
