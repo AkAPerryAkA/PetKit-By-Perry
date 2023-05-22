b04c70875ec4830204a060986e5cd1b9afb8d4e3
AkAPerryAkA <AkAPerryAkA@Gmail.com>
2023-05-22 19:59:30 +0000

    modified:   custom_components/petkit_by_perry/config_flow.py 	renamed:    custom_components/petkit_by_perry/translations.json/en.json -> custom_components/petkit_by_perry/translations/en.json

--- custom_components/petkit_by_perry/config_flow.py
+++ custom_components/petkit_by_perry/config_flow.py
@@ -30,7 +30,7 @@ class PetKitByPerryConfigFlow(config_entries.ConfigFlow, domain = DOMAIN):
                 vol.Required('Username'): TextSelector(TextSelectorConfig(type=TextSelectorType.EMAIL, autocomplete="username")),
                 vol.Required('Password'): TextSelector(TextSelectorConfig(type=TextSelectorType.PASSWORD, autocomplete="current-password")),
                 vol.Required("Country", default=dict(API_COUNTRY).get(str(Locale(getCountryCode(str(tzlocal.get_localzone())).replace('_', '-').upper())))): vol.In(sorted(list(dict(API_COUNTRY).values()))),
-                vol.Required("Timezone", default=dict(API_TIMEZONE).get(str(tzlocal.get_localzone()))): vol.In(sorted(list(dict(API_TIMEZONE).values()))),
+                vol.Required("Timezone", default=dict(API_TIMEZONE).get(str(Locale(getCountryCode(str(tzlocal.get_localzone())).replace('_', '-').upper())))): vol.In(sorted(list(dict(API_TIMEZONE).values()))),
             }
         )
 
