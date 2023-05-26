e2c1bce2a8a325327f131bd7233374ff98c1c44a
AkAPerryAkA <AkAPerryAkA@Gmail.com>
2023-05-26 20:26:10 +0000

    modified:   custom_components/petkit_by_perry/__init__.py

--- custom_components/petkit_by_perry/__init__.py
+++ custom_components/petkit_by_perry/__init__.py
@@ -1,6 +1,7 @@
 # IMPORTS #
 import logging
 from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -10,13 +11,21 @@ from .config_flow import *
 from .Account import Account
 from .Device import *
 
-async def async_setup(hass: HomeAssistant, hass_config: dict) -> bool:
-    hass.data.setdefault(DOMAIN, {})
-    config = hass_config.get(DOMAIN) or {}
-    if config is None:
-        return False
-    for Acc in config:
-        t_acc = Account(hass, Acc)
-        if t_acc.update_token():
-            t_acc.get_devices()
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    device = PetKit(hass, entry.data[DOMAIN])
+    await device.async_setup()
+
+class PetKit:
+    def __init__(self, hass: HomeAssistant, config: dict):
+        self._config = config
+        self.hass = hass
+    async def async_setup(self) -> bool:
+        self.hass.data.setdefault(DOMAIN, {})
+        config = self._config.get(DOMAIN) or {}
+        if config is None:
+            return False
+        for Acc in config:
+            t_acc = Account(self.hass, Acc)
+            if t_acc.update_token():
+                t_acc.get_devices()
             
