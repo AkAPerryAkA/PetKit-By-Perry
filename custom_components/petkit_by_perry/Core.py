26fe55a08731e2842722e00fc30beaefbb3e611d
AkAPerryAkA <AkAPerryAkA@Gmail.com>
2023-05-29 17:07:00 +0000

    modified:   custom_components/petkit_by_perry/Core.py

--- custom_components/petkit_by_perry/Core.py
+++ custom_components/petkit_by_perry/Core.py
@@ -34,68 +34,61 @@ async def getAPIServers():
                 API_TIMEZONE.append([list(CountryCode.values())[2].upper(), TimeZone])
 
 async def getAPIToken(Username, Password, Country, TimeZone):
-    try:
-        if re.findall(r"([a-fA-F\d]{32})", Password):
-            Password = Password.lower()
-        else:
-            hash = hashlib.md5()
-            hash.update(Password.encode("utf-8"))
-            Password = hash.hexdigest()
-        TimeZone = pytz.timezone(TimeZone)
-        Param = {
-            "timezoneId": TimeZone.zone,
-            "timezone": f"{round(TimeZone._utcoffset.seconds/60/60)}.0",
-            "username": Username,
-            "password": Password,
-            "locale": locale.getdefaultlocale()[0],
-            "encrypt": 1,
-        }
-        Result = await sendRequest(None, TimeZone, list(dict(API_SERVERS).get(dict(API_COUNTRY).keys()).index(list(dict(API_COUNTRY).values()).index(Country))) + API_LOGIN_PATH, Param)
-        Account = {
-            "UserID": Result['user']['account']['userId'],
-            "Username": Username,
-            "Password": Password,
-            "Country": Country,
-            "TimeZone": str(TimeZone),
-            "Token": Result['session']['id'],
-            "Token_Created": str(datetime.strptime(Result['session']["createdAt"], "%Y-%m-%dT%H:%M:%S.%fZ")),
-            "Token_Expires": str(datetime.strptime(Result['session']["createdAt"], "%Y-%m-%dT%H:%M:%S.%fZ") + timedelta(seconds = Result['session']["expiresIn"]))
-        }
-        return Account
-    except Exception as error:
-        _LOGGER.error('Token request failed: %s', error)
+    if re.findall(r"([a-fA-F\d]{32})", Password):
+        Password = Password.lower()
+    else:
+        hash = hashlib.md5()
+        hash.update(Password.encode("utf-8"))
+        Password = hash.hexdigest()
+    TimeZone = pytz.timezone(TimeZone)
+    Param = {
+        "timezoneId": TimeZone.zone,
+        "timezone": f"{round(TimeZone._utcoffset.seconds/60/60)}.0",
+        "username": Username,
+        "password": Password,
+        "locale": locale.getdefaultlocale()[0],
+        "encrypt": 1,
+    }
+    Result = await sendRequest(None, TimeZone, dict(API_SERVERS).get(list(dict(API_COUNTRY).keys()).index(list(dict(API_COUNTRY).values()).index(Country))) + API_LOGIN_PATH, Param)
+    Account = {
+        "UserID": Result['user']['account']['userId'],
+        "Username": Username,
+        "Password": Password,
+        "Country": Country,
+        "TimeZone": str(TimeZone),
+        "Token": Result['session']['id'],
+        "Token_Created": str(datetime.strptime(Result['session']["createdAt"], "%Y-%m-%dT%H:%M:%S.%fZ")),
+        "Token_Expires": str(datetime.strptime(Result['session']["createdAt"], "%Y-%m-%dT%H:%M:%S.%fZ") + timedelta(seconds = Result['session']["expiresIn"]))
+    }
+    return Account
 
 async def sendRequest(Account, TimeZone, URL, Param = None):
-    try:
-        if Account is not None:
-            if Account.token_expires > datetime.now():
-                await getAPIToken(None, None, None, None)
-            Header = {
-                "X-Session": Account.token,
-            }
+    if Account is not None:
+        if Account.token_expires > datetime.now():
+            await getAPIToken(None, None, None, None)
+        Header = {
+            "X-Session": Account.token,
+        }
+    else:
+        Header = {}
+    Header.update({
+        "User-Agent": "PETKIT/7.26.1 (iPhone; iOS 14.7.1; Scale/3.00)",
+        "X-Timezone": f"{round(TimeZone._utcoffset.seconds/60/60)}.0",
+        "X-Api-Version": "7.26.1",
+        "X-Img-Version": "1",
+        "X-TimezoneId": TimeZone.zone,
+        "X-Client": "ios(14.7.1;iPhone13,4)",
+        "X-Locale": locale.getdefaultlocale()[0].replace("-", "_"),
+    })
+    async with aiohttp.ClientSession(headers=Header) as session:
+        async with session.post(url=URL, params=Param) as response:
+            result = await response.json()
+    if list(result.keys())[0] == 'result':
+        if list(result['result'])[0] == 'list':
+            return result['result']['list']
         else:
-            Header = {}
-        Header.update({
-            "User-Agent": "PETKIT/7.26.1 (iPhone; iOS 14.7.1; Scale/3.00)",
-            "X-Timezone": f"{round(TimeZone._utcoffset.seconds/60/60)}.0",
-            "X-Api-Version": "7.26.1",
-            "X-Img-Version": "1",
-            "X-TimezoneId": TimeZone.zone,
-            "X-Client": "ios(14.7.1;iPhone13,4)",
-            "X-Locale": locale.getdefaultlocale()[0].replace("-", "_"),
-        })
-        async with aiohttp.ClientSession(headers=Header) as session:
-            async with session.post(url=URL, params=Param) as response:
-                result = await response.json()
-        if list(result.keys())[0] == 'result':
-            if list(result['result'])[0] == 'list':
-                return result['result']['list']
-            else:
-                return result['result']
-        elif list(result.keys())[0] == 'error':
-            raise ValueError(result['error']['msg'])
-        else:
-            raise ValueError('Unknown error!')
-    except Exception as error:
-        detailederror = [URL, error]
-        _LOGGER.error('API request failed: %s', detailederror)
+            return result['result']
+    elif list(result.keys())[0] == 'error':
+        raise ValueError(result['error']['msg'])
+    else:
+        raise ValueError('Unknown error!')
