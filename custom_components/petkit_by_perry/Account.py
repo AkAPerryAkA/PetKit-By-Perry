ca8e737af4a06b3e66fc25f9bad59d1e1674dd19
AkAPerryAkA <AkAPerryAkA@Gmail.com>
2023-05-29 23:35:22 +0000

    modified:   custom_components/petkit_by_perry/Account.py

--- custom_components/petkit_by_perry/Account.py
+++ custom_components/petkit_by_perry/Account.py
@@ -97,7 +97,7 @@ class Account:
     
     async def send_request(self, URL, Param = None, Token = False) -> dict:
         if Token is True:
-            if datetime.strptime(self.token_expires, "%Y-%m-%d %H:%M:%S.%f") > datetime.now():
+            if datetime.strptime(self.token_expires, "%Y-%m-%d %H:%M:%S.%f") < datetime.now():
                 await self.update_token()
             Header = {
                 "X-Session": self.token,
